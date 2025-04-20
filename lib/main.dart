62a5fab379bebd1c4290a744cc0a5a2adb9a4c9a
Alex Marin <Gohantk@gmail.com>
2025-04-20 09:46:26 -0500

    Basic Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,title: 'Hola Mundo', home: RecipeBook());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola Mundo',
+      home: RecipeBook(),
+    );
   }
 }
 
@@ -16,6 +20,36 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: Colors.orange,title: Text('Recipe Book', style: TextStyle(color: Colors.white),)),);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(width: MediaQuery.of(context).size.width, height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(),),
+              ),
+              SizedBox(width: 26,),
+              Column(children: <Widget>[
+                Text('Lasagna'),
+                Text('Alex Marin'),
+                Container(
+                  height: 2,
+                  width: 75,
+                  color: Colors.orange,
+                )
+              ],)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
