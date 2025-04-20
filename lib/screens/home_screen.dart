cf801112729087fb88def5f4ad06babb19bd44e2
Alex Marin <Gohantk@gmail.com>
2025-04-20 13:25:56 -0500

    Imagenes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,49 +6,54 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        _RecipesCard(context),
-        _RecipesCard(context)
-      ],)
+      body: Column(
+        children: <Widget>[_RecipesCard(context), _RecipesCard(context)],
+      ),
     );
   }
 
-Widget _RecipesCard(BuildContext context){
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Container(
-      width: MediaQuery.of(context).size.width, height: 125,
+  Widget _RecipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
         child: Card(
           child: Row(
             children: <Widget>[
               Container(
                 height: 125,
                 width: 100,
-                decoration: BoxDecoration(
+                child: ClipRRect(
                   borderRadius: BorderRadius.circular(12),
-                  color: Colors.orange
+                  child: Image.network(
+                    'https://newmansown.com/wp-content/uploads/2022/03/Homemade-lasagna.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-              SizedBox(width: 26,),
+              SizedBox(width: 26),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                Text('Lasagna', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
-                SizedBox(height: 4,),
-                Text('Alex Marin', style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),),
-                Container(
-                  height: 2,
-                  width: 75,
-                  color: Colors.orange,
-                ),
-                SizedBox(height: 4,)
-              ],)
+                  Text(
+                    'Lasagna',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'Alex Marin',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  Container(height: 2, width: 75, color: Colors.orange),
+                  SizedBox(height: 4),
+                ],
+              ),
             ],
           ),
         ),
       ),
-  );
-}
-
+    );
+  }
 }
